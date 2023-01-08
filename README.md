9d7c05c8791331b04485f8ca3f1921097a122b70
julianregatky <julianregatky@gmail.com>
2023-01-08 14:57:34 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# splitwiser
+# DebtSolver
+
+A tool to solve debt allocation problems. Given a dictionary of expenses, the solver will determine how much each person owes or is owed in order to even out the expenses.
+
+## Mathematical Formulation
+
+The debt allocation problem can be formulated as an integer linear programming problem as follows:
+
+Let $n$ be the number of people, $E_i$ be the amount spent by person $i$, and $D_i$ be the amount that person $i$ owes. The problem can be written as:
+
+$$
+\begin{aligned}
+    \text{minimize} \quad & \sum_{i=1}^{n} \sum_{j=1}^{n} x_{i,j} \\
+    \text{subject to} \quad & \sum_{i=1}^{n} z_{i,j} = L_j \quad & \forall j \in \{1, \dots, n\} \\
+                            & \sum_{j=1}^{n} z_{i,j} = D_i \quad & \forall i \in \{1, \dots, n\} \\
+                            & Mx_{i,j} - z_{i,j} \ge 0 \quad & \forall i, j \in \{1, \dots, n\} \\
+                            & x_{i,j}, z_{i,j} \in \mathbb{Z} \quad & \forall i, j \in \{1, \dots, n\} \\
+\end{aligned}
+$$
+
+where $L_j$ is the amount that person $j$ is owed, and $x_{i,j}$ and $z_{i,j}$ are binary and integer variables, respectively. The first constraint ensures that each person gets back the amount that they are owed, and the second constraint ensures that each person pays back their debts. The third constraint is a big-M constraint that ensures that a debt is only assigned if it is non-zero.
+
+The third constraint is known as a big-M constraint, where $M$ is a large positive constant. This constraint is used to ensure that a debt is only assigned if it is non-zero. Specifically, if $x_{i,j} = 1$, then the debt $z_{i,j}$ must be non-zero, and if $x_{i,j} = 0$, then the debt $z_{i,j}$ must be zero. The large value of $M$ ensures that the constraint is always satisfied when $x_{i,j} = 1$.
+
+## Usage
+
+To use the DebtSolver, first install OR-Tools:
+
+```bash
+pip install ortools
+```
+
+Then, create an instance of the `DebtSolver` class with a dictionary of expenses and call the `solve` method.
+
+```python
+import collections
+from debt_solver import DebtSolver
+
+expenses = collections.OrderedDict(zip(['Person A', 'Person B', 'Person C'], [100, 50, 0]))
+result = DebtSolver(expenses).solve()
+```
---
+++ solver.py
@@ -0,0 +1,92 @@
+from ortools.linear_solver import pywraplp
+
+class DebtSolver:
+    """
+    A class to solve debt allocation problems. Given a dictionary of expenses, the solver will
+    determine how much each person owes or is owed in order to even out the expenses.
+    """
+    def __init__(self, expenses):
+        """
+        Initializes the DebtSolver with a dictionary of expenses.
+
+        Args:
+            expenses: A dictionary of expenses where the keys are the names of the people and the
+                      values are the amounts they spent.
+        """
+        self.expenses, self.per_capita_expense = self.round_cents(expenses)
+        self.debts = {k: max(0, self.per_capita_expense-v) for k,v in self.expenses.items()}
+        self.loans = {k: max(0, v-self.per_capita_expense) for k,v in self.expenses.items()}
+
+        self.solver = pywraplp.Solver.CreateSolver('SCIP')
+        
+        # Create variables
+        self.variables = {}
+        for person_i in self.expenses.keys():
+            for person_j in self.expenses.keys():
+                name = f"x_{person_i}_{person_j}"
+                self.variables[name] = self.solver.IntVar(0, 1, name)
+                name = f"z_{person_i}_{person_j}"
+                self.variables[name] = self.solver.IntVar(0, self.solver.infinity(), name)
+
+        # Add constraints
+        for person_j in self.expenses.keys():
+            self.solver.Add(sum(self.variables[f"z_{person_i}_{person_j}"] for person_i in self.expenses.keys()) == self.loans[person_j])
+        for person_i in self.expenses.keys():
+            self.solver.Add(sum(self.variables[f"z_{person_i}_{person_j}"] for person_j in self.expenses.keys()) == self.debts[person_i])
+        for person_i in self.expenses.keys():
+            for person_j in expenses.keys():
+                self.solver.Add(999999999.0 * self.variables[f"x_{person_i}_{person_j}"] - self.variables[f"z_{person_i}_{person_j}"] >= 0)
+
+        # Set objective
+        objective = self.solver.Sum(self.variables[f"x_{person_i}_{person_j}"] for person_i in self.expenses.keys() for person_j in self.expenses.keys())
+        self.solver.Minimize(objective)
+
+    def solve(self):
+        """
+        Solves the debt allocation problem and returns a string representation of the solution.
+        
+        Returns:
+            A string representation of the solution. If a solution is found, it will be in the form
+            "*Debts:*\n\n[person] owes [person]: [amount]\n[person] owes [person]: [amount]\n...".
+            If a solution is not found, it will return "I couldn't find a solution to the problem.".
+        """
+        status = self.solver.Solve()
+        if status == pywraplp.Solver.OPTIMAL:
+            message = "*Debts:*\n\n"
+            for person_i in self.expenses.keys():
+                for person_j in self.expenses.keys():
+                    debt = self.variables[f"z_{person_i}_{person_j}"].solution_value()
+                    if debt > 0:
+                        message = f"{message}{person_i} owes {person_j}: ${int(debt)}\n"
+            return message.rstrip('\n')
+        else:
+            return "Couldn't find a solution to the problem."
+
+    @staticmethod
+    def round_cents(expenses):
+        """
+        Rounds the expenses to the nearest dollar and returns the rounded expenses and the per-capita
+        expense.
+
+        Args:
+            expenses: A dictionary of expenses where the keys are the names of the people and the
+                      values are the amounts they spent.
+
+        Returns:
+            A tuple containing the rounded expenses as a dictionary and the per-capita expense as an
+            integer.
+        """
+        # Round the per-capita expense to the nearest dollar
+        per_capita_expense = round(sum(expenses.values())/len(expenses))
+
+        # Calculate the difference between the total expenses and the per-capita expense
+        diff = sum(expenses.values())-(per_capita_expense*len(expenses))
+        
+        # Sort the expenses in decreasing order by value
+        expenses = sorted(list(expenses.items()), key=lambda x: x[1], reverse=True)
+        
+        # Adjust the largest expense to compensate for the difference
+        expenses[0] = (expenses[0][0], expenses[0][1]-diff)
+        
+        # Return the rounded expenses and per-capita expense
+        return dict(expenses), per_capita_expense
